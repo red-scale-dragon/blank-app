d1c93da37d6cdf555f1d670ce151786575764cba
Rick Mac Gillis <noreply@rickmacgillis.com>
2025-04-08 12:52:28 -0700

    Updates
    Signed-off-by: Rick Mac Gillis <noreply@rickmacgillis.com>

--- app/Http/Requests/Admin/AdminSettingsRequest.php
+++ app/Http/Requests/Admin/AdminSettingsRequest.php
@@ -6,10 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 use Dragon\Support\User;
 
 class AdminSettingsRequest extends FormRequest {
-	public function authorize(): bool {
-		return is_admin() && User::isAdmin();
-	}
-	
 	public function messages(): array {
 		return [
 			'remove_migrations_on_deactivation.in' => 'Remove DB tables on deactivation must be yes or no.',
